c38d5e0a06b09b47f83335498c70592bf3053d8f
arturkowalczyk300 <arturkowalczyk300@gmail.com>
2024-10-30 22:12:01 +0100

    add MainWindow E2E tests

--- CompanyFleetManagerDesktopAppTestsE2E/MainWindowTests.cs
+++ CompanyFleetManagerDesktopAppTestsE2E/MainWindowTests.cs
@@ -1,35 +1,83 @@
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Windows;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support.UI;
+using Xunit;
+
 namespace CompanyFleetManagerDesktopAppTestsE2E
 {
     public class MainWindowTests
     {
-        [Fact]
-        public void MainWindowTest_OnLoad_ShouldRentalTabBeActive()
+        private const string AppPath = @"C:\Users\artur\source\repos\CompanyFleetManagerDesktopApp\CompanyFleetManagerDesktopApp\bin\Debug\net8.0-windows\CompanyFleetManagerDesktopApp.exe";
+        private WindowsDriver<WindowsElement> _driver;
+
+        public MainWindowTests()
         {
-            throw new NotImplementedException();
+            var options = new AppiumOptions();
+            options.AddAdditionalCapability("app", AppPath);
+            options.AddAdditionalCapability("deviceName", "WindowsPC");
+
+            _driver = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723"), options);
         }
 
         [Fact]
-        public void MainWindowTest_OnLoad_ShouldDisplayRentals()
+        public void MainWindowTest_OnLoad_ShouldRentalTabBeActive()
         {
-            throw new NotImplementedException();
+            var rentalTab = _driver.FindElementByAccessibilityId("TabItemRentals");
+
+            Assert.True(rentalTab.Selected, "The Rental tab should be active on MainWindow load.");
         }
 
         [Fact]
         public void MainWindowTest_OnClickVehiclesTab_ShouldDisplayVehiclesTab()
         {
-            throw new NotImplementedException();
+            _driver.Manage().Window.Maximize();
+
+            var vehiclesTab = _driver.FindElementByAccessibilityId("TabItemVehicles");
+
+            vehiclesTab.Click();
+
+            var wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(2));
+            wait.Until(d => vehiclesTab.Selected);
+
+            Assert.True(vehiclesTab.Selected, "The Vehicles tab should be active after click Vehicles tab switch button");
         }
 
         [Fact]
         public void MainWindowTest_OnClickEmployeesTab_ShouldDisplayEmployeesTab()
         {
-            throw new NotImplementedException();
+            _driver.Manage().Window.Maximize();
+
+            var employeesTab = _driver.FindElementByAccessibilityId("TabItemEmployees");
+
+            employeesTab.Click();
+
+            var wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(2));
+            wait.Until(d => employeesTab.Selected);
+
+            Assert.True(employeesTab.Selected, "The Employees tab should be active after click Employees tab switch button");
         }
 
         [Fact]
         public void MainWindowTest_OnClickRentalsTab_ShouldDisplayRentalsTab()
         {
-            throw new NotImplementedException();
+            _driver.Manage().Window.Maximize();
+
+            var vehiclesTab = _driver.FindElementByAccessibilityId("TabItemVehicles");
+            vehiclesTab.Click();
+            var wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(2));
+            wait.Until(d => vehiclesTab.Selected);
+            Assert.True(vehiclesTab.Selected, "The Vehicles tab should be active after click Vehicles tab switch button");
+
+
+            var rentalsTab = _driver.FindElementByAccessibilityId("TabItemRentals");
+
+            rentalsTab.Click();
+
+            wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(2));
+            wait.Until(d => rentalsTab.Selected);
+
+            Assert.True(rentalsTab.Selected, "The Rentals tab should be active after click Rentals tab switch button");
         }    
     }
 }
